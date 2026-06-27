4daa4c703d77721399180bc8ad516e1dd37546e8
HanifnB <mihanif06@gmail.com>
2026-06-27 15:13:31 +0700

    Deply Vercel dengan DB Aiven

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,11 @@
 <?php
+$host = "mysql-keuangan-tugaskeuangancrud01.l.aivencloud.com";
+$user = "avnadmin";
+$pass = "AVNS_e9ZydT4EUJuCpvQOvaS"; 
+$db   = "defaultdb"; 
+$port = "11531"; 
 
-$conn = mysqli_connect(
-    "localhost",
-    "root",
-    "",
-    "keuangan_db"
-);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal : " . mysqli_connect_error());
